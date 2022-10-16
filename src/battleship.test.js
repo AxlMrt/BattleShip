f42f0a589f70ef01ed3b9fc849177344f2e2ac5d
Axel <eldrank@gmail.com>
2022-10-16 10:21:01 +0200

    Adjust test

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -4,3 +4,7 @@ import { Ship } from './battleship.factory';
 test('Create a ship object', () => {
   expect(Ship(5)).toEqual({ length: 5, health: 5, isDrown: false });
 });
+
+test('Create a ship object', () => {
+  expect(Ship(2)).toEqual({ length: 2, health: 2, isDrown: false });
+});
